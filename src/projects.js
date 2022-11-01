649a6b5bd3225b85690d801241e1ff5739cbd417
Rodhi Faisal Mufid <111524042+Rodhi2@users.noreply.github.com>
2022-11-01 21:21:11 +0700

    Add files via upload

---
+++ src/projects.js
@@ -0,0 +1,40 @@
+
+const projects = [
+	{
+		id: 1,
+		title: 'SDF Cambodia',
+		category: 'Web Application',
+		
+	},
+	{
+		id: 2,
+		title: 'SAHAKA',
+		category: 'Web Application',
+		
+	},
+	{
+		id: 3,
+		title: 'APD Website',
+		category: 'API',
+		
+	},
+	{
+		id: 4,
+		title: 'Staff Management',
+		category: 'Microservice',
+	
+	{
+		id: 5,
+		title: 'SportDate',
+		category: 'Mobile Application',
+	
+	},
+	{
+		id: 6,
+		title: 'POS Camcyber',
+		category: 'Web Application',
+		
+	},
+];
+
+export default projects;
---
+++ src/views/Contact.vue
@@ -0,0 +1,50 @@
+<script>
+import feather from 'feather-icons';
+import ContactForm from '@/components/contact/ContactForm.vue';
+import ContactDetails from '@/components/contact/ContactDetails.vue';
+
+export default {
+	components: {
+		ContactForm,
+		ContactDetails,
+	},
+	data: () => {
+		return {
+			contacts: [
+				{
+					id: 1,
+					name: 'Bandar Lampung, Indonesia',
+					icon: 'map-pin',
+				},
+				{
+					id: 2,
+					name: 'rodhifaisalmufid2@gmail.com',
+					icon: 'mail',
+				},
+				{
+					id: 3,
+					name: '+62 852 6674 5599',
+					icon: 'phone',
+				},
+			],
+		};
+	},
+	mounted() {
+		feather.replace();
+	},
+	updated() {
+		feather.replace();
+	},
+	methods: {},
+};
+</script>
+
+<template>
+	<div
+		class="container mx-auto flex flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
+	>
+		<ContactForm />
+
+		<ContactDetails :contacts="contacts" />
+	</div>
+</template>
